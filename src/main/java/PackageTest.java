028881ab98bfa67797ca431e0cf6aa74f0dc57e1
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 02:13:42 +0300

    Scripted auto-commit on change (2022-02-22 02:13:42) by gitwatch.sh

--- src/main/java/PackageTest.java
+++ src/main/java/PackageTest.java
@@ -1,8 +1,17 @@
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "packId",
+        "jsScript",
+        "funcName",
+        "test"
+})
 public class PackageTest {
     @JsonProperty("packId") private String packId;
-    @JsonProperty("packId") private String packId;
-    @JsonProperty("packId") private String packId;
-    @JsonProperty("packId") private String packId;
+    @JsonProperty("jsScript") private String jsScript;
+    @JsonProperty("funcName") private String funcName;
+    @JsonProperty("test") private List<> test;
 }
