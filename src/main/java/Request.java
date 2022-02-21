e77a44c2a0ed4b440da9baef01bebbc8ada1727d
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 00:41:28 +0300

    Scripted auto-commit on change (2022-02-22 00:41:28) by gitwatch.sh

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -1,5 +1,9 @@
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Request implements Serializable {
-    
+    private String packId;
+    private String jsScript;
+    private String funcName;
+    private ArrayList<Test> tests;
 }
