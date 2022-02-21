2b801438262697c07f30c83e9c835d6193f26dda
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 01:34:40 +0300

    Scripted auto-commit on change (2022-02-22 01:34:40) by gitwatch.sh

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Result {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Result implements Serializable {
+    private String packId;
+    private ArrayList<String> results;
+
+    public Result(String packId, ArrayList<String> results) {
+        this.packId = packId;
+        this.results = results;
+    }
+
+    public String getPackId() {
+        return packId;
+    }
+
+    public ArrayList<String> getResults() {
+        return results;
+    }
 }
