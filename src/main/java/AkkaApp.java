8cc0927d533312d270bed25391092d3733999e33
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 00:05:19 +0300

    first

---
+++ src/main/java/AkkaApp.java
@@ -0,0 +1,10 @@
+import akka.actor.Actor;
+import akka.actor.ActorSystem;
+import akka.actor.ActorSystemImpl;
+
+public class AkkaApp {
+    public static void main(String[] args) {
+        ActorSystem actorsystem = ActorSystem.create("AkkaApp");
+
+    }
+}
