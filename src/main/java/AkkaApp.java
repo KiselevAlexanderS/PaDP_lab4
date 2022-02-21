4b17d5f9f4158a8e3230ca35d6beb622c36d8803
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 02:19:31 +0300

    Scripted auto-commit on change (2022-02-22 02:19:31) by gitwatch.sh

--- src/main/java/AkkaApp.java
+++ src/main/java/AkkaApp.java
@@ -1,11 +1,10 @@
 import akka.NotUsed;
-import akka.actor.Actor;
-import akka.actor.ActorSystem;
-import akka.actor.ActorSystemImpl;
+import akka.actor.*;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
@@ -15,6 +14,10 @@ public class AkkaApp extends AllDirectives {
         final Http http = Http.get(actorsystem);
         final ActorMaterializer materia = ActorMaterializer.create(actorsystem);
         AkkaApp akkaApp = new AkkaApp();
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = akkaApp.c
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = akkaApp.createRoute()
+    }
+
+    private Route createRoute(ActorSystem system) {
+        ActorRef testsRouter = system.actorOf(Props.create(Router.class, Router::new));
     }
 }
