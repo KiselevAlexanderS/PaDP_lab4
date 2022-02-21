12029d347bafc1c90c3c0515abe83023840c6973
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 02:15:01 +0300

    Scripted auto-commit on change (2022-02-22 02:15:01) by gitwatch.sh

--- src/main/java/AkkaApp.java
+++ src/main/java/AkkaApp.java
@@ -1,8 +1,9 @@
 import akka.actor.Actor;
 import akka.actor.ActorSystem;
 import akka.actor.ActorSystemImpl;
+import akka.http.javadsl.server.AllDirectives;
 
-public class AkkaApp {
+public class AkkaApp extends AllDirectives {
     public static void main(String[] args) {
         ActorSystem actorsystem = ActorSystem.create("AkkaApp");
 
