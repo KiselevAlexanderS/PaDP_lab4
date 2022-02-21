b5789271f919986b3d480d008dcf774a85b20325
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 00:52:16 +0300

    Scripted auto-commit on change (2022-02-22 00:52:16) by gitwatch.sh

--- src/main/java/ResultActor.java
+++ src/main/java/ResultActor.java
@@ -3,6 +3,7 @@ import akka.actor.ActorRef;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class ResultActor extends AbstractActor {
 
@@ -12,7 +13,7 @@ public class ResultActor extends AbstractActor {
         this.storage = storage;
     }
 
-    private String runTest(Test test) {
+    private String runTest(Test test) throws ScriptException {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
         engine.eval(test.getParent().getJsScript());
     }
