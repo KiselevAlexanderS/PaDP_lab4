f654b9a218af034c841ac1f5d89cdb708c9cb90a
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 00:46:20 +0300

    Scripted auto-commit on change (2022-02-22 00:46:20) by gitwatch.sh

--- src/main/java/ResultActor.java
+++ src/main/java/ResultActor.java
@@ -1,6 +1,14 @@
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 
 public class ResultActor extends AbstractActor {
+
+    private ActorRef storage;
+
+    ResultActor(ActorRef storage) {
+        this.storage = storage;
+    }
+    
 
     @Override
     public Receive createReceive() {
