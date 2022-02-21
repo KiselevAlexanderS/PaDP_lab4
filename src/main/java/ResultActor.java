47757555a5e73a7abfd19910996ca780e6ea2f81
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 00:53:06 +0300

    Scripted auto-commit on change (2022-02-22 00:53:06) by gitwatch.sh

--- src/main/java/ResultActor.java
+++ src/main/java/ResultActor.java
@@ -16,6 +16,8 @@ public class ResultActor extends AbstractActor {
     private String runTest(Test test) throws ScriptException {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
         engine.eval(test.getParent().getJsScript());
+
+        return 
     }
 
     @Override
