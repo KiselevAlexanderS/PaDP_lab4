a64c592351717de4f0a99e01a58d9ada6205606c
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 02:29:25 +0300

    Scripted auto-commit on change (2022-02-22 02:29:25) by gitwatch.sh

--- src/main/java/PackageTest.java
+++ src/main/java/PackageTest.java
@@ -1,8 +1,11 @@
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
@@ -16,4 +19,5 @@ public class PackageTest {
     @JsonProperty("jsScript") private String jsScript;
     @JsonProperty("funcName") private String funcName;
     @JsonProperty("test") private List<SingleTest> test;
+    @JsonIgnore private Map<String,Object> unexpextedProps = new HashMap<>();
 }
