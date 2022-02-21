a98f89fa5260e2d2f90719d7c822af8342284a4c
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 01:19:27 +0300

    Scripted auto-commit on change (2022-02-22 01:19:27) by gitwatch.sh

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -8,6 +8,11 @@ public class Storage extends AbstractActor {
 
     private Map<String, ArrayList<Test>> store = new HashMap<>();
 
+    private void input(Test test) {
+        String packId = test.getParent().getPackId();
+
+        
+    }
 
     @Override
     public Receive createReceive() {
