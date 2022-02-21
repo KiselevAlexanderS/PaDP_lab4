250e832c1d26bb1c24fcb9d33a2ff3ea5b5be8a5
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 00:39:45 +0300

    Scripted auto-commit on change (2022-02-22 00:39:45) by gitwatch.sh

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,37 @@
 import java.io.Serializable;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Data implements Serializable {
-    
+public class Test implements Serializable {
+    private String testname;
+    private String expectedResult;
+    private Object[] params;
+    private String result;
+
+    @JsonCreator
+    Test(@JsonProperty("testname")String testname, @JsonProperty("expectedResult") String expectedResult, @JsonProperty("params") Object[] params) {
+        this.testname = testname;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    public Object[] getParams() {
+        return params;
+    }
+
+    public String getTestname() {
+        return testname;
+    }
+
+    public String getResult() {
+        return result;
+    }
 }
