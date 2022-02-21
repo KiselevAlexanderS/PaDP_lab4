7275de0ef69abfdbe1ffa509d91f0e3bf885c9e1
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-22 01:48:39 +0300

    Scripted auto-commit on change (2022-02-22 01:48:39) by gitwatch.sh

---
+++ src/main/java/PackageResult.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class PackageResult {
+    private String packId;
+    private ArrayList<String> result;
+
+    PackageResult(String packId, ArrayList<String> result) {
+        this.packId = packId;
+        this.result = result;
+    }
+
+    public String getPackId() {
+        return packId;
+    }
+
+    public ArrayList
+}
